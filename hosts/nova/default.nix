35371b6162bc882a91a5228b53ef926a841d658a
Louis Bailleau <louis13.bailleau@gmail.com>
2024-09-23 09:50:02 +0200

    feat: configure grub options

--- hosts/nova/default.nix
+++ hosts/nova/default.nix
@@ -16,13 +16,28 @@
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot";
     };
     grub = {
       enable = true;
       device = "nodev";
       efiSupport = true;
-      useOSProber = true;
+      useOSProber = false;
+      gfxmodeEfi = "2880x1880";
+      extraEntries = ''
+menuentry 'Arch Linux' --class arch --class gnu-linux --class gnu --class os $menuentry_id_option 'gnulinux-simple-522d414c-a3eb-4746-8682-8a851c57eb07' {
+	insmod part_gpt
+	insmod fat
+	search --no-floppy --fs-uuid --set=root 7BC6-FEFD
+	linux /vmlinuz-linux root=/dev/dm-0
+	initrd /initramfs-linux.img
+}
+menuentry "Reboot" {
+	reboot
+}
+menuentry "Poweroff" {
+	halt
+}
+      '';
     };
   };
 
