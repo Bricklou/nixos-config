3f7d9fd86163d6a929266218aa6c1091153294fa
Louis Bailleau <louis13.bailleau@gmail.com>
2024-12-26 23:49:38 +0100

    feat: remove vscode useless config

--- home/linux/gui/plasma/apps.nix
+++ home/linux/gui/plasma/apps.nix
@@ -19,20 +19,6 @@ in {
       enable = true;
       # let vscode sycnc and update its configurations & extensions across devices, using github account
       userSettings = {};
-      package =
-        pkgs.vscode.override
-        {
-          # https://wiki.archlinux.org/title/Wayland#Electron
-          commandLineArgs = [
-            "--ozone-platform-hint=auto"
-            "--ozone-platform=wayland"
-            # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it
-            # (only supported by chromium/chrome at this time, not electron)
-            "--gtk-version=4"
-            # make it use text-input-v1, which works for kwin 5.27 and weston
-            "--enable-wayland-ime"
-          ];
-        };
     };
   };
 }
