87c47f31de2bff9707d1667d4ec39dae556444f4
Louis Bailleau <louis13.bailleau@gmail.com>
2024-12-28 17:36:54 +0100

    feat: configure nameservers

--- modules/base/networking.nix
+++ modules/base/networking.nix
@@ -12,4 +12,24 @@
       userServices = true;
     };
   };
+
+  networking.nameservers = [
+    "2606:4700:4700::1111"
+    "2606:4700:4700::1001"
+    "1.1.1.1"
+    "1.0.0.1"
+  ];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = ["~."];
+    fallbackDns = [
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
+    ];
+    dnsovertls = "true";
+  };
 }
