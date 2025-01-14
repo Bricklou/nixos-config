e171820094dbfe3f15db49a546111d208aebc502
Louis Bailleau <louis13.bailleau@gmail.com>
2025-01-14 08:22:20 +0100

    feat: add winapps cachix substituer

--- modules/desktop/winapps.nix
+++ modules/desktop/winapps.nix
@@ -1,6 +1,12 @@
 {winapps, ...}: let
   system = "x86_64-linux";
 in {
+  # set up binary caches
+  nix.settings = {
+    substituters = ["https://winapps.cachix.org"];
+    trusted-public-keys = ["winapps.cachix.org-1:HI82jWrXZsQRar/PChgIx1unmuEsiQMQq+zt05CD36g="];
+  };
+
   environment.systemPackages = [
     winapps.packages.${system}.winapps
     winapps.packages.${system}.winapps-launcher
