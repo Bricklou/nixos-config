8d48c39789b56ec1447e889d12d1c4fa35f859a2
Louis Bailleau <louis13.bailleau@gmail.com>
2025-05-12 20:58:14 +0200

    feat: let vscode manage its stuff

--- home/linux/gui/plasma/apps.nix
+++ home/linux/gui/plasma/apps.nix
@@ -2,9 +2,8 @@
   programs.vscode = {
     enable = true;
     # let vscode sync and update its configurations & extensions across devices, using github account
-    profiles.default.userSettings = {
-      "workbench.iconTheme" = "material-icon-theme";
-    };
+    profiles.default.userSettings = {};
+    profiles.web.userSettings = {};
     profiles.ALE.userSettings = {};
   };
 }
