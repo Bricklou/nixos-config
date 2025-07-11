5b2eba55cde6c9001660448b7c12805d766ca652
Louis Bailleau <louis13.bailleau@gmail.com>
2025-07-11 11:54:31 +0200

    chore: update flake dependencies

--- modules/desktop/packages.nix
+++ modules/desktop/packages.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: {
-  boot.loader.timeout = lib.mkForce 10; #wait for x seconds to select the boot entry.
+  boot.loader.timeout = lib.mkForce 10; # wait for x seconds to select the boot entry.
 
   environment.systemPackages =
     (with pkgs; [
@@ -29,21 +29,19 @@
       firefox
       epiphany
     ])
-    ++ (with pkgs-stable; [
+    ++ (with pkgs; [
       (microsoft-edge.override {
         # convert array into string for command lines
-        commandLineArgs =
-          lib.concatStringsSep " "
-          [
-            "--ignore-gpu-blocklist"
-            "--enable-zero-copy"
-            "--gtk-version=3"
-            "--use-gl=angle"
-            "--use-angle=gl"
-            "--enable-features=UseMultiPlaneFormatForHardwareVideo,VaapiVideoDecodeLinuxGL,VaapiVideoEncodeLinuxGL"
-            "--enable-features=AcceleratedVideoDecoder,AcceleratedVideoEncoder"
-            "--ozone-platform=wayland"
-          ];
+        commandLineArgs = lib.concatStringsSep " " [
+          "--ignore-gpu-blocklist"
+          "--enable-zero-copy"
+          "--gtk-version=3"
+          "--use-gl=angle"
+          "--use-angle=gl"
+          "--enable-features=UseMultiPlaneFormatForHardwareVideo,VaapiVideoDecodeLinuxGL,VaapiVideoEncodeLinuxGL"
+          "--enable-features=AcceleratedVideoDecoder,AcceleratedVideoEncoder"
+          "--ozone-platform=wayland"
+        ];
       })
     ]);
 }
