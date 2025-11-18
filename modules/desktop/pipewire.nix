f91eff0a4a0e18cad9ef2b72dcee5c45d4920a40
Louis Bailleau <louis13.bailleau@gmail.com>
2025-11-18 08:56:22 +0100

    feat: try to configure rnnoise plugin for pipewire

---
+++ modules/desktop/pipewire.nix
@@ -0,0 +1,46 @@
+{pkgs, ...}: {
+  services.pipewire = {
+    extraLv2Packages = [pkgs.rnnoise-plugin];
+
+    extraConfig.pipewire = {
+      "99-input-denoising.conf" = {
+        context.modules = [
+          {
+            name = "libpipewire-module-filter-chain";
+            args = {
+              node.name = "rnnoise_source";
+              node.description = "Noise Canceling source";
+              media.name = "Noise Canceling source";
+              filter.graph = {
+                nodes = [
+                  {
+                    type = "ladspa";
+                    name = "rnnoise";
+                    plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                    label = "noise_suppressor_mono";
+                    control = {
+                      "VAD Threshold (%)" = 50.0;
+                      "VAD Grace Period (ms)" = 200;
+                      "Retroactive VAD Grace (ms)" = 0;
+                    };
+                  }
+                ];
+              };
+              "capture.props" = {
+                "node.passive" = true;
+              };
+              "playback.props" = {
+                "media.class" = "Audio/Source";
+              };
+            };
+          }
+        ];
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    rnnoise-plugin
+    easyeffects
+  ];
+}
