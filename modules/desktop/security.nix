b5298caaf668c73f9a00172529d6e20a55f904ab
Louis Bailleau <louis13.bailleau@gmail.com>
2026-02-21 15:28:15 +0100

    feat: configure yubi key

--- modules/desktop/security.nix
+++ modules/desktop/security.nix
@@ -1,15 +1,36 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   # security with polkit
   security.polkit.enable = true;
 
   # Enable YubiKey packages
-  services.udev.packages = [pkgs.yubikey-personalization];
+  # https://joinemm.dev/blog/yubikey-nixos-guide
+  services = {
+    pcscd.enable = true;
+    udev.packages = [pkgs.yubikey-personalization];
+  };
 
-  security.pam.yubico = {
-    enable = true;
-    debug = true;
-    mode = "challenge-response";
-    id = ["32855598"];
+  security.pam = {
+    u2f = {
+      enable = true;
+      settings = {
+        interactive = true;
+        cue = true;
+
+        origin = "pam://yubi";
+        authfile = pkgs.writeText "u2f-mappings" (lib.concatStrings [
+          ":javYbqsH1wawZ1GanIcC7lr5zPeFXYaCXUfz9VRqoCI7X47KqNrQmjW5/46TqiQxCwcXhjPBKISqGx7FWSU/0w==,sSAbdgq04dFW2oBBDPijOiK2fTVztzLw+geywVfp6JxGwt/syKlNJspF7chgF0sN9gzKkt7Rm6ozuKNUEItn6w==,es256,+p"
+        ]);
+      };
+    };
+
+    services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+    };
   };
 
   # gpg agent with pinentry
